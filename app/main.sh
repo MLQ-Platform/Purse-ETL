2ab25f68741c14cd81394b70480cb628b3049202
Yang-Hyun-Jun <sugumy@naver.com>
2025-05-10 15:17:42 +0900

    update

--- app/main.sh
+++
@@ -1,1 +0,0 @@
-nohup uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload > uvicorn.log 2>&1 &
--- etl/loader/__init__.py
+++
@@ -1,5 +0,0 @@
-from etl.loader.abstract import BaseLoader
-from etl.loader.sql_loader import SQLDatabaseLoader
-from etl.loader.redis_loader import RedisDatabaseLoader
-
-__all__ = ["BaseLoader", "SQLDatabaseLoader", "RedisDatabaseLoader"]
